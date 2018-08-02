c2155e8273b872f1df03185a15a4f748f82a2659
Yao <GyonGyon@users.noreply.github.com>
2018-08-03 00:23:23 +0800

    update: sort
    heap

--- array_sort.py
+++ array_sort.py
@@ -41,3 +41,56 @@ def bubble(array):
                 a[i], a[i + 1] = a[i + 1], a[i]
             i += 1
         max -= 1
+
+
+def heap_parent(index):
+    return (index + 1) // 2
+
+
+def heap_left(index):
+    i = 2 * (index + 1) - 1
+    return i
+
+
+def heap_right(index):
+    return heap_left(index) + 1
+
+
+def heap_max_heapify(array, index, heap_size):
+    a = array
+    size = heap_size
+    i = index
+    if i < size:
+        l = heap_left(i)
+        r = heap_right(i)
+    else:
+        return
+    if l < size and a[l] > a[i]:
+        largest = l
+    else:
+        largest = i
+    if r < size and a[r] > a[largest]:
+        largest = r
+    if not (largest == i):
+        a[i], a[largest] = a[largest], a[i]
+        heap_max_heapify(a, largest, size)
+
+
+def heap_build_max_heap(array):
+    a = array
+    size = len(a)
+    length = size // 2
+    for i in range(length - 1, -1, -1):
+        heap_max_heapify(a, i, size)
+
+
+def heap(array):
+    a = array
+    heap_build_max_heap(a)
+    size = len(a)
+    length = size - 1
+    i = size - 1
+    for i in range(length, 0, -1):
+        a[i], a[0] = a[0], a[i]
+        size -= 1
+        heap_max_heapify(a, 0, size)
