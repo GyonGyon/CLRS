6393df26f874a442b61b94ff608e48804889ee95
Yao <yao@cryptape.com>
2018-08-22 21:09:35 +0800

    update: add pre_order_recur

--- tree.py
+++ tree.py
@@ -3,3 +3,13 @@ class TreeNode:
         self.value = n
         self.left = None
         self.right = None
+
+
+def pre_order_recur(node, callback):
+    if node == None:
+        return
+    callback(node)
+    if node.left != None:
+        pre_order_recur(node.left, callback)
+    if node.right != None:
+        pre_order_recur(node.right, callback)
